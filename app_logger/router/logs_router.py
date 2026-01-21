c21c73ca33a84a021c53da3674e471b59323b1eb
gorkapereza-lab <gorka.pereza@alumni.mondragon.edu>
2026-01-21 12:57:21 +0100

    ip influxbd hardcodeada

--- app_logger/router/logs_router.py
+++ app_logger/router/logs_router.py
@@ -12,46 +12,46 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/logs/private", tags=["logs"])
 
-INFLUX_URL: Optional[str] = None
+INFLUX_URL = os.getenv("INFLUXDB_URL", "http://10.1.12.50:5000")
 INFLUX_TOKEN = os.getenv("INFLUXDB_TOKEN", "MY_CUSTOM_TOKEN_123456")
 INFLUX_ORG = os.getenv("INFLUXDB_ORG", "my-org")
 
 # Cache interno (simple)
-_influx_client: Optional[InfluxDBClient] = None
-_influx_query_api = None
-_influx_lock = asyncio.Lock()
+# _influx_client: Optional[InfluxDBClient] = None
+# _influx_query_api = None
+# _influx_lock = asyncio.Lock()
 
-# client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
-# query_api = client.query_api()
+client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
+query_api = client.query_api()
 
 
-async def _get_query_api():
-    """
-    Devuelve query_api inicializado una sola vez.
+# async def _get_query_api():
+#     """
+#     Devuelve query_api inicializado una sola vez.
 
-    - Resuelve INFLUX_URL vía Consul solo la primera vez.
-    - Crea InfluxDBClient solo la primera vez.
-    - Reutiliza el mismo query_api en llamadas posteriores.
-    """
-    global INFLUX_URL, _influx_client, _influx_query_api
+#     - Resuelve INFLUX_URL vía Consul solo la primera vez.
+#     - Crea InfluxDBClient solo la primera vez.
+#     - Reutiliza el mismo query_api en llamadas posteriores.
+#     """
+#     global INFLUX_URL, _influx_client, _influx_query_api
 
-    if _influx_query_api is not None:
-        return _influx_query_api
+#     if _influx_query_api is not None:
+#         return _influx_query_api
 
-    async with _influx_lock:
-        if _influx_query_api is not None:
-            return _influx_query_api
+#     async with _influx_lock:
+#         if _influx_query_api is not None:
+#             return _influx_query_api
 
-        # Aquí sí puedes usar await (ya estás dentro de una async def)
-        INFLUX_URL = await get_consul_client().get_service_base_url("logs_inf")
+#         # Aquí sí puedes usar await (ya estás dentro de una async def)
+#         INFLUX_URL = await get_consul_client().get_service_base_url("logs_inf")
 
-        _influx_client = InfluxDBClient(
-            url=INFLUX_URL,
-            token=INFLUX_TOKEN,
-            org=INFLUX_ORG,
-        )
-        _influx_query_api = _influx_client.query_api()
-        return _influx_query_api
+#         _influx_client = InfluxDBClient(
+#             url=INFLUX_URL,
+#             token=INFLUX_TOKEN,
+#             org=INFLUX_ORG,
+#         )
+#         _influx_query_api = _influx_client.query_api()
+#         return _influx_query_api
 
 @router.get("/")
 async def root():
@@ -99,7 +99,7 @@ async def get_errors(
           |> sort(columns: ["_time"], desc: true)
         '''
 
-        query_api = await _get_query_api()
+        # query_api = await _get_query_api()
         result = query_api.query(query)
 
         data = []
@@ -148,7 +148,7 @@ async def get_monitoring(
           |> sort(columns: ["_time"], desc: true)
         '''
 
-        query_api = await _get_query_api()
+        # query_api = await _get_query_api()
         result = query_api.query(query)
 
         data = []
@@ -190,7 +190,7 @@ async def get_debug(
           |> sort(columns: ["_time"], desc: true)
         '''
 
-        query_api = await _get_query_api()
+        # query_api = await _get_query_api()
         result = query_api.query(query)
 
         data = []
